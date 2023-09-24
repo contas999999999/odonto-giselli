c8397d13ecc68059e49588848a7b26a2e1480293
contas999999999 <126794902+contas999999999@users.noreply.github.com>
2023-09-24 16:28:32 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -33,6 +33,7 @@ export default function Footer(){
           <div className='location'>
             <h4>ENDEREÇO</h4>
             <span>Rua Brasil 1170, sala 52 - Edifício Emídio Borges Rudge Ramos, São Bernado do Campo</span>
+                 </div>
           <div className='Estacionamento'>
             <h4>Estacionamento</h4>
             <span> Estacionamento gratuito</span>
