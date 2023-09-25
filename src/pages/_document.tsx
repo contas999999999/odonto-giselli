13702d49e9b01ebb27cd2487ed05f3c55445fc27
contas999999999 <126794902+contas999999999@users.noreply.github.com>
2023-09-25 12:54:12 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,21 +5,7 @@ export default function Document() {
 
   return (
     <Html lang="pt-br">
-      <Head>
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S6CJ4JVQTX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-S6CJ4JVQTX');
-</script>
-      </Head>
-      <body >
-        <Main />
-        <NextScript />
-      </body>
+     <body/>
     </Html>
   )
 }
